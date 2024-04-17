7142d9d8eee8e8d90bf946e38b0b1a10ee6a440a
J. Klesley <klesley.pfeitosa@gmail.com>
2024-04-17 19:30:03 -0300

    Commit Inicial

---
+++ crud_estoque.c
@@ -0,0 +1,38 @@
+/* A proposta é fazer um controle de estoque de um armazém que abriga produtos diversos
+para revenda. Até o momento, pensamos em implementar as opções:
+
+		- Adicionar produto
+		- Retirar produto
+		- Modificar informação
+		- Excluir produto
+		
+Isso será feito utilizando-se da TAD lista dinâmica encadeada.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <malloc.h>
+
+typedef struct produto{
+	char id[20];
+	char nome[70];
+	float pesoUni;
+	int quantidade;
+	float precoUni;
+	float precoTotal;
+} pdt;
+
+int main(void) {
+	setlocale(LC_ALL, "Portuguese");
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
