d60c02fd6b16cb5fc89f72e87e9f5ed7d073df15
J. Klesley <klesley.pfeitosa@gmail.com>
2024-04-17 21:50:05 -0300

    feat: Implementação de um sistema básico de login

--- crud_estoque.c
+++ crud_estoque.c
@@ -5,6 +5,7 @@ para revenda. Até o momento, pensamos em implementar as opções:
 		- Retirar produto
 		- Modificar informação
 		- Excluir produto
+		- Histórico de ações
 		
 Isso será feito utilizando-se da TAD lista dinâmica encadeada.
 */
@@ -13,25 +14,69 @@ Isso será feito utilizando-se da TAD lista dinâmica encadeada.
 #include <stdio.h>
 #include <locale.h>
 #include <malloc.h>
+#include <string.h>
+#include <time.h>
 
-typedef struct produto{
+typedef struct pdt{
 	char id[20];
 	char nome[70];
 	float pesoUni;
 	int quantidade;
 	float precoUni;
 	float precoTotal;
-} pd;
+} dadosProduto;
+/* Cada nó da lista corresponderá a uma struct pdt e um ponteiro. */
+
+typedef struct listaP{
+	dadosProduto produto;
+	struct listaP* proximo;
+} listaP;
+
+void autenticacao(char* nomeUser){
+	char nome[50], password[20];
+	int t = 3;
+	
+	printf("|| Informe o nome de usuário: ");
+	gets(nome);
+	do{
+		printf("|| Informe a senha de acesso: ");
+		gets(password);
+		if(strcmp(password, "arm2024storage") != 0){
+			t--;
+			if(t == 0) break;
+			printf("(!) Restam %d tentativas.\n", t);
+		}
+	} while(strcmp(password, "arm2024storage") != 0 && t!=0);
+	
+	if(t == 0){
+		system("cls");
+		puts("~ Você zerou o número de tentativas. O programa será encerrado. ~");
+		exit(1);
+	}
+	else{
+		puts("-----------------------------------------------------");
+		strcpy(nomeUser, nome);
+	 	puts(">> Login realizado COM SUCESSO.");
+	 	printf("- Usuário: %s\n", nomeUser);
+	 	time_t t; time(&t);
+		printf("- Dados de login: %s", ctime(&t));
+		
+	 	system("pause");
+	 	system("cls");
+	}
+}
+
 
 int main(void) {
 	setlocale(LC_ALL, "Portuguese");
 	
+	char userNome[50];
+	int status;
 	
-	
-	
-	
-	
-	
+	puts("========================================================================");
+	puts("      BEM-VINDO AO SISTEMA DE CONTROLE DE ESTOQUE Almeida Storage!      ");
+	puts("========================================================================");
+	autenticacao(userNome);
 	
 	return 0;
 }
